5c8c34bb629cbccb96e4f1cb4d10ce149798af2f
Juan Pedro <juanpedrovsc@gmail.com>
2024-05-22 21:37:49 -0300

    Adicionando JoinColumn

--- ProjetoFinalAPI/src/main/java/br/com/serratec/entity/Pedido.java
+++ ProjetoFinalAPI/src/main/java/br/com/serratec/entity/Pedido.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
@@ -19,7 +20,8 @@ public class Pedido {
 	private Long id;
 
 	private LocalDateTime dataPedido;
-
+	
+	@JoinColumn(name = "id_cliente")
 	@ManyToOne
 	private Cliente cliente;
 
--- ProjetoFinalAPI/src/main/java/br/com/serratec/entity/Produto.java
+++ ProjetoFinalAPI/src/main/java/br/com/serratec/entity/Produto.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -21,7 +22,7 @@ public class Produto {
 
 	@Enumerated(EnumType.STRING)
 	private CategoriaEnum categoria;
-
+	@JoinColumn(name = "id_pedido")
 	@ManyToOne
 	private Pedido pedido;
 
