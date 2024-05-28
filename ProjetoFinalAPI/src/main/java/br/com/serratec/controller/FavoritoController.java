c1ba39f8177b9b44ab7af9f2c7bcdfe9139c3fea
jbasts <jonasbf.gomes@gmail.com>
2024-05-28 16:41:34 -0300

    Inserir,deletar e atualizar

--- ProjetoFinalAPI/src/main/java/br/com/serratec/controller/FavoritoController.java
+++ ProjetoFinalAPI/src/main/java/br/com/serratec/controller/FavoritoController.java
@@ -1,8 +1,10 @@
 package br.com.serratec.controller;
 
-import java.util.List;
+import java.util.List
+;
 
 //import java.util.List;
+
 //
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.http.HttpStatus;
@@ -95,8 +97,13 @@ import java.util.List;
 //    }
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -104,6 +111,11 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.serratec.dto.FavoritoDTO;
 import br.com.serratec.entity.Favorito;
 import br.com.serratec.service.FavoritoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/favoritos")
@@ -120,4 +132,24 @@ public class FavoritoController {
     public List<FavoritoDTO> listarFavoritos() {
         return favoritoService.listarTodos();
     }
+    @PutMapping("/{id}")
+  @Transactional
+  @Operation(summary = "Atualiza um favorito existente", description = "Atualização de Favorito")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Favorito atualizado com sucesso"),
+      @ApiResponse(responseCode = "404", description = "Favorito não encontrado"),
+  })
+  public ResponseEntity<String> atualizar(@PathVariable Long id, @Valid @RequestBody Favorito favorito) {
+      return  favoritoService.atualizar(id, favorito);
+  }
+
+  @DeleteMapping("/{id}")
+  @Operation(summary = "Deleta um favorito existente", description = "Exclusão de Favorito")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204", description = "Favorito deletado com sucesso"),
+      @ApiResponse(responseCode = "404", description = "Favorito não encontrado"),
+  })
+  public ResponseEntity<String> deletar(@PathVariable Long id) {
+      return favoritoService.deletar(id);
+      }
 }
--- ProjetoFinalAPI/src/main/java/br/com/serratec/service/FavoritoService.java
+++ ProjetoFinalAPI/src/main/java/br/com/serratec/service/FavoritoService.java
@@ -1,99 +1,66 @@
 package br.com.serratec.service;
 
-import java.util.List;import java.util.stream.Collector;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.serratec.dto.FavoritoDTO;
 import br.com.serratec.entity.Favorito;
+import br.com.serratec.exception.ResourceNotFoundException;
 import br.com.serratec.repository.FavoritoRepository;
-
-//@Service
-//public class FavoritoService {
-//
-//	@Autowired
-//	private FavoritoRepository repository;
-//	
-//	@Autowired
-//	private ProdutoService produtoService;
-//	
-//	
-//
-//	// listarc
-//	public List<FavoritoDTO> listar() {
-//		List <Favorito> favoritos = repository.findAll();
-//		
-//		
-//		 return favoritos.stream().map((p) -> new FavoritoDTO(p)).collect(Collectors.toList());
-//	}
-//	
-//	public FavoritoDTO listarId(Long id) {
-//	    Favorito favorito = repository.findById(id)
-//	            .orElseThrow(() -> new ResourceNotFoundException("Favorito não encontrado!"));
-//	    FavoritoDTO favoritoResponseDTO = new FavoritoDTO(favorito);
-//
-//	    return favoritoResponseDTO;
-//	}
-//	
-//
-//	// inserir
-//	  public FavoritoDTO inserir(@Valid FavoritoRequestDTO favoritoRequestDTO) {
-//	        Favorito favorito = new Favorito();
-//	        
-//	        favorito.setCliente(favoritoRequestDTO.getCliente());
-//	        favorito.setStatus(favoritoRequestDTO.getStatus());
-//	        
-//	        Set<Carrinho> carrinhos = new HashSet<>();
-//	        
-//	        
-//	        for (Long produtoId : favoritoRequestDTO.getProdutoIds()) {
-//	            Produto produto = produtoService.listarId(produtoId);
-//	            
-//	            carrinhos.add(new Carrinho(favorito, produto));
-//	        }
-//	        favorito.setCarrinhos(carrinhos);
-//	        repository.save(favorito);
-//	        FavoritoDTO response = new FavoritoDTO(favorito);
-//	        return response;
-//	    }
-//
-//	// atualizar
-//
-//	public Favorito atualizar(Long id, @Valid Favorito favorito) {
-//		if (repository.existsById(id)) {
-//			favorito.setId(id);
-//			repository.save(favorito);
-//		}
-//		throw new ResourceNotFoundException("Favorito com o id: " + id + " não encontrado!");
-//	}
-//
-//	// deletar
-//	public ResponseEntity<String> deletar(Long id) {
-//		if (repository.existsById(id)) {
-//			repository.deleteById(id);
-//			return ResponseEntity.status(HttpStatus.SC_OK).body("Favorito deletado com sucesso!");
-//		}
-//		throw new ResourceNotFoundException("Favorito com o id: " + id + " não encontrado!");
-//	}
-//}
+import jakarta.validation.Valid;
 
 @Service
 public class FavoritoService {
-    @Autowired
-    private FavoritoRepository favoritoRepository;
-    
- 
-    public FavoritoDTO salvar(Favorito favorito) {
-    	FavoritoDTO dto = new FavoritoDTO(favorito);
-        favoritoRepository.save(favorito);
-        return dto;
-    }
+	@Autowired
+	private FavoritoRepository favoritoRepository;
 
-    
-    public List<FavoritoDTO> listarTodos() {
-    	 List<Favorito> fav = favoritoRepository.findAll();
-    	 return fav.stream().map((f)->new FavoritoDTO(f)).collect(Collectors.toList());
-    }
+	public FavoritoDTO salvar(Favorito favorito) {
+		FavoritoDTO dto = new FavoritoDTO(favorito);
+		favoritoRepository.save(favorito);
+		return dto;
+	}
+
+	public List<FavoritoDTO> listarTodos() {
+		List<Favorito> fav = favoritoRepository.findAll();
+		return fav.stream().map((f) -> new FavoritoDTO(f)).collect(Collectors.toList());
+	}
+
+	public ResponseEntity<String> atualizar(Long id, @Valid Favorito favorito) {
+		FavoritoDTO dto = new FavoritoDTO(favorito);
+		if (favoritoRepository.existsById(id)) {
+			favorito.setId(id);
+			favoritoRepository.save(favorito);
+			return ResponseEntity.status(HttpStatus.OK).body("Informações Atualizadas com sucesso!");
+		}
+		throw new ResourceNotFoundException("Usuário com o id: " + id + " não encontrado!");
+	}
+	
+	public ResponseEntity<String> deletar(Long id) {
+		if (favoritoRepository.existsById(id)) {
+			favoritoRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Favorito deletado com sucesso!");
+		}
+		throw new ResourceNotFoundException("Favorito com o id: " + id + " não encontrado!");
+	}	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
